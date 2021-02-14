a9dfe79c0b2dbfbc7a0805434d96b5dd156e58a8
Eve <46225510+eveOfCode@users.noreply.github.com>
2021-02-13 22:26:21 -0400

    fixed progress ui and added donation task ui

--- layouts/userDashboard.vue
+++ layouts/userDashboard.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-main>
       <v-container class="background" fluid fill-height>
-        <v-layout row wrap justify-center align-center fill-height>
+        <v-layout row wrap justify-start align-start fill-height>
           <nuxt />
         </v-layout>
       </v-container>
--- pages/userDashboard.vue
+++ pages/userDashboard.vue
@@ -1,5 +1,48 @@
 <template>
-  <div><h1>User Dashboard</h1></div>
+  <v-container>
+    <h1>User Dashboard</h1>
+    <br />
+    <v-layout justify-start align-start row wrap>
+      <v-flex lg6>
+        <!-- Avatar and user progress -->
+        <v-card flat class="pa-4">
+          <v-container>
+            <v-layout row wrap>
+              <v-flex xs4>
+                <v-img src="./boyAvatar.jpg"></v-img>
+              </v-flex>
+              <v-flex xs8>
+                <h4>
+                  Level 2
+                  <v-progress-linear value="80" rounded height="25">
+                    <strong>80%</strong>
+                  </v-progress-linear>
+                </h4>
+                <h4>100 Points</h4>
+                <h4>5 Gold Coins</h4>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+        <v-divider></v-divider>
+      </v-flex>
+    </v-layout>
+    <!-- Donation tasks -->
+    <v-layout row wrap>
+      <v-flex lg4 class="mt-4">
+        <v-card elevation="2" class="">
+          <v-card-title>Donate Zafia a winter jacket 🧥</v-card-title>
+          <v-card-subtitle>Gain 5 points</v-card-subtitle>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn rounded class="primary ma-3">$10</v-btn>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <!-- ----------------------------- -->
+  </v-container>
 </template>
 
 <script>
